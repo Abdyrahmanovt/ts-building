7855f49cea6bb48cc3970ff0f59d6f5734d49576
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2021-12-23 20:14:05 +0600

    update project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route,Routes} from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./view/Home";
+import About from "./view/About";
+import Contacts from "./view/Contacts";
 
 
 
@@ -10,6 +12,9 @@ const App = () => {
         <div className="app">
             <Header/>
             <Routes>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/about' component={About}/>
+                <Route exact path='/contacts' component={Contacts}/>
                 <Route exact path='/' component={Home}/>
             </Routes>
         </div>
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,7 +6,7 @@ import {Link} from "react-router-dom";
 const Header = () => {
     return (
         <header>
-            <div className="header">
+            <div className="container header">
                 <div className="logo">
                     logo
                 </div>
