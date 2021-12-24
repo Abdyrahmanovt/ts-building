53d3352e6f980adc7ea0335c837f0f7d782d39e9
Abdyrahmanov Timurlan <timurlanabdyrahmanov7@gmail.com>
2021-12-24 20:45:13 +0600

    update project

--- src/view/Contacts/index.js
+++ src/view/Contacts/index.js
@@ -19,7 +19,7 @@ const Contacts = () => {
                     </div>
                 </div>
                 <div className="contact-map">
-                    <iframe width="700" height="300" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"
+                    <iframe title="map" width="700" height="300" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"
                             src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=IHLAS%20Residence,%20%D0%B6%D0%B8%D0%BB%D0%BE%D0%B9%20%D0%BA%D0%BE%D0%BC%D0%BF%D0%BB%D0%B5%D0%BA%D1%81+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"/>
                 </div>
             </div>
